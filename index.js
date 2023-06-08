0692f696ccd6944d9922702737b9208f564fa604
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-08 20:52:24 +0600

    using post method to send login user data in mongodb

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// Middle ware 
+// Middle ware
 app.use(cors());
 app.use(express.json());
 
-
+// MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvcivem.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,28 +18,44 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("focusStudio").collection("users");
+
+    
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send([]);
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Focus Studio server!!!')
-})
+app.get("/", (req, res) => {
+  res.send("Focus Studio server!!!");
+});
 
 app.listen(port, () => {
-  console.log(`Focus Studio is running on port ${port}`)
-})
+  console.log(`Focus Studio is running on port ${port}`);
+});
