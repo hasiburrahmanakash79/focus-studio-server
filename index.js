c13f3f94ff101fa2e001d4687697570b75b67c2d
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-09 21:37:24 +0600

    verify jwt access token

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// JWT token verify
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  //bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvcivem.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,18 +51,29 @@ async function run() {
 
     const usersCollection = client.db("focusStudio").collection("users");
     const classesCollection = client.db("focusStudio").collection("classes");
-    const instructorsCollection = client.db("focusStudio").collection("instructors");
+    const instructorsCollection = client
+      .db("focusStudio")
+      .collection("instructors");
 
-    app.get("/users", async(req, res) => {
-      const result = await usersCollection.find().toArray()
-      res.send(result)
-    })
-    
+    //JWT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
+      if (existingUser) {
         return res.send([]);
       }
       const result = await usersCollection.insertOne(user);
@@ -48,17 +81,18 @@ async function run() {
     });
 
     // classes
-    app.get("/classes", async(req, res) => {
-      const result = await classesCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get("/instructors", async(req, res) => {
-      const result = await instructorsCollection.find().toArray()
-      res.send(result)
-    })
+    // Instructors 
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
+    });
 
-    // Make Admin 
+    // Make Admin
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filterId = { _id: new ObjectId(id) };
@@ -71,7 +105,7 @@ async function run() {
       res.send(result);
     });
 
-    // Make instructor 
+    // Make instructor
     app.patch("/users/instructor/:id", async (req, res) => {
       const id = req.params.id;
       const filterId = { _id: new ObjectId(id) };
