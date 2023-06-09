6a8fc4ed3a64b70f92c7e924219bc6fd6ed6ca52
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-09 18:34:34 +0600

    make admin from user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -56,6 +56,19 @@ async function run() {
       const result = await instructorsCollection.find().toArray()
       res.send(result)
     })
+
+    // Make Admin 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filterId = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filterId, updateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
